8c3397211f3627706676b79d87b9fa5b91af9fb4
Geoffrey Huntley <ghuntley@ghuntley.com>
2014-12-29 08:34:54 +1100

    nancyfx works

---
+++ ravendb-nancyfx-ioc-repro/Modules/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace ravendb_nancyfx_ioc_repro.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
